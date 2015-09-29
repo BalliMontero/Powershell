10ce113a7628173173a13798278234f38f87270e
benpturner <benpturner@yahoo.com>
2015-09-29 11:21:13 +0100

    Changed location

--- Invoke-WMIFileSearch.ps1
+++ Invoke-WMIFileSearch.ps1
@@ -190,10 +190,10 @@ function Invoke-WMIFileSearchIP
     {
         Write-Host `n'Host is alive: ' $IPAddress
 
-        $wmiquery = "SELECT * FROM CIM_DataFile WHERE Drive ='C:' AND Path='\\windows\\' AND Extension='exe' AND CreationDate > '05/01/2015' "
-        Get-WmiObject -Query $wmiquery -ComputerName $IPAddress -Credential($getcreds) | foreach {Write-Host $_.Name}
+        $wmiquery = "SELECT * FROM CIM_DataFile WHERE Drive ='C:' AND Extension='exe' AND CreationDate > '10/03/2015' "
+        #Get-WmiObject -Query $wmiquery -ComputerName $IPAddress -Credential($getcreds) | foreach {Write-Host $_.Name}
 
-        #Get-WmiObject -Query $wmiquery -ComputerName $IPAddress -Credential($getcreds) | where-object Name -match '[a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z].exe' |foreach {Write-Host $_.Name}
+        Get-WmiObject -Query $wmiquery -ComputerName $IPAddress -Credential($getcreds) | where-object Name -match '[a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z].exe' |foreach {Write-Host $_.Name}
     }   
 }
 

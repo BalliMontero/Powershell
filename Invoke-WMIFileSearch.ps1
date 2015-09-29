dcea8523c84b769ead57f1c4878adacdfcae3450
benpturner <benpturner@yahoo.com>
2015-09-29 10:48:36 +0100

    New

--- Invoke-WMIFileSearch.ps1
+++ Invoke-WMIFileSearch.ps1
@@ -1,3 +1,73 @@
+<#
+.Synopsis
+    Generates a IP Address Objects for IPv4 and IPv6 Ranges.
+.DESCRIPTION
+    Generates a IP Address Objects for IPv4 and IPv6 Ranges given a ranges in CIDR or
+    range <StartIP>-<EndIP> format.
+.EXAMPLE
+    PS C:\> New-IPvRange -Range 192.168.1.1-192.168.1.5
+
+    Generate a collection of IPv4 Object collection for the specified range.
+
+.EXAMPLE
+   New-IPRange -Range 192.168.1.1-192.168.1.50 | select -ExpandProperty ipaddresstostring
+
+   Get a list of IPv4 Addresses in a given range as a list for use in another tool.
+#>
+function New-IPRange
+{
+    [CmdletBinding(DefaultParameterSetName="CIDR")]
+    Param(
+        [parameter(Mandatory=$true,
+        ParameterSetName = "CIDR",
+        Position=0)]
+        [string]$CIDR,
+
+        [parameter(Mandatory=$true,
+        ParameterSetName = "Range",
+        Position=0)]
+        [string]$Range   
+    )
+    if($CIDR)
+    {
+        $IPPart,$MaskPart = $CIDR.Split("/")
+        $AddressFamily = ([System.Net.IPAddress]::Parse($IPPart)).AddressFamily
+
+        # Get the family type for the IP (IPv4 or IPv6)
+        $subnetMaskObj = [IPHelper.IP.Subnetmask]::Parse($MaskPart, $AddressFamily)
+        
+        # Get the Network and Brodcast Addressed
+        $StartIP = [IPHelper.IP.IPAddressAnalysis]::GetClasslessNetworkAddress($IPPart, $subnetMaskObj)
+        $EndIP = [IPHelper.IP.IPAddressAnalysis]::GetClasslessBroadcastAddress($IPPart,$subnetMaskObj)
+        
+        # Ensure we do not list the Network and Brodcast Address
+        $StartIP = [IPHelper.IP.IPAddressAnalysis]::Increase($StartIP)
+        $EndIP = [IPHelper.IP.IPAddressAnalysis]::Decrease($EndIP)
+        [IPHelper.IP.IPAddressAnalysis]::GetIPRange($StartIP, $EndIP)
+    }
+    elseif ($Range)
+    {
+        $StartIP, $EndIP = $range.split("-")
+        [IPHelper.IP.IPAddressAnalysis]::GetIPRange($StartIP, $EndIP)
+    }
+}
+
+<#
+.Synopsis
+    Generates a list of IPv4 IP Addresses given a CIDR.
+.DESCRIPTION
+    Generates a list of IPv4 IP Addresses given a CIDR.
+.EXAMPLE
+    Generating a list of IPs
+    PS C:\> New-IPv4RangeFromCIDR -Network 192.168.1.0/29
+    192.168.1.1
+    192.168.1.2
+    192.168.1.3
+    192.168.1.4
+    192.168.1.5
+    192.168.1.6
+    192.168.1.7
+#>
 function New-IPv4RangeFromCIDR 
 {
     param(

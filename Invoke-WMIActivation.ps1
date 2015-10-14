a676b7a4670358a9d05cd6048dfd25dc505b8ef2
benpturner <benpturner@yahoo.com>
2015-10-14 17:37:33 +0100

    New

--- Invoke-WMIActivation.ps1
+++ Invoke-WMIActivation.ps1
@@ -188,40 +188,9 @@ function Invoke-WMIActivationIP
     $ping = New-Object System.Net.NetworkInformation.Ping
     if (($ping.send($IPAddress, 10)).Status-eq 'Success')
     {
-
         Write-Host `n'Host is alive: ' $IPAddress
-
-        try {
-            $wpa = Get-WmiObject SoftwareLicensingProduct -ComputerName $IPAddress `
-            -Filter "ApplicationID = '55c92734-d682-4d71-983e-d6ec3f16059f'" `
-            -Property LicenseStatus -ErrorAction Stop -Credential($getcreds)
-        } catch {
-            $status = New-Object ComponentModel.Win32Exception ($_.Exception.ErrorCode)
-            $wpa = $null    
-        }
-
-        $out = New-Object psobject -Property @{
-            ComputerName = $IPAddress;
-            Status = [string]::Empty;
-        }
-        if ($wpa) {
-            :outer foreach($item in $wpa) {
-                switch ($item.LicenseStatus) {
-                    0 {$out.Status = "Unlicensed"}
-                    1 {$out.Status = "Licensed"; break outer}
-                    2 {$out.Status = "Out-Of-Box Grace Period"; break outer}
-                    3 {$out.Status = "Out-Of-Tolerance Grace Period"; break outer}
-                    4 {$out.Status = "Non-Genuine Grace Period"; break outer}
-                    5 {$out.Status = "Notification"; break outer}
-                    6 {$out.Status = "Extended Grace"; break outer}
-                    default {$out.Status = "Unknown value"}
-                }
-            }
-        } else {$out.Status = $status.Message}
-        $out
-    
-
         
+        Get-WmiObject SoftwareLicensingProduct | where PartialProductKey | select Name, ApplicationId, LicenseStatus | Format-List *
      }
            
 }
